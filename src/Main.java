c17ee25d1e51810e53574bb9ef1b143a461d8811
Jon Mockbee <mockbee1000@gmail.com>
2019-07-11 11:16:03 -0400

    adding testing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    // find this on exercise 38 at approx 6:04
+    public static void main(String[] args) {
+        boolean isbarking =BarkingDog.shouldWakeUp(true,23);
+        System.out.println("is the dog barking? = "+ isbarking );
+    }
 }
